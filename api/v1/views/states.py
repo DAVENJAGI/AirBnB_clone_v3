103cad3aaa68667f22c19b1c63713653e27431fd
DAVENJAGI <njagidave39@gmail.com>
2024-01-08 17:17:35 +0300

    state_update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -43,36 +43,30 @@ def return_state(state_id):
         storage.delete(all_states)
         return jsonify(all_states.to_dict()), 200
 
+    elif request.method == "POST":
+        if not request.get_json():
+            abort(400, description="Not a JSON")
 
-@state_views.route("/states/", strict_slashes=False, methods=["POST"])
-def post_state():
-    """posts a new state"""
-    if not request.get_json():
-        abort(400, description="Not a JSON")
+        if 'name' not in request.get_json():
+            abort(400, description="Missing name")
 
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
+        state_name = request.get_json()
+        instance = State(**state_name)
+        instance.save()
+        return jsonify(instance.to_dict()), 201
 
-    state_name = request.get_json()
-    instance = State(**state_name)
-    instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+    else:
+        all_states = storage.get(State, state_id)
+        if not all_states:
+            abort(404)
+        if not request.get_json():
+            abort(400, description="Not a JSON")
 
+        ignore = ['id', 'creates_at', 'updated_at']
 
-@state_views.route("/states/<state_id>", methods=["PUT"])
-def update_state(state_id):
-    """updates data on a state"""
-    all_states = storage.get(State, state_id)
-    if not all_states:
-        abort(404)
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-
-    ignore = ['id', 'creates_at', 'updated_at']
-
-    data = request.get_json()
-    for key, value in data.items():
-        if key not in ignore:
-            setattr(state, key, value)
-    storage.save()
-    return make_response(jsonify(state.to_dict()), 200)
+        data = request.get_json()
+        for key, value in data.items():
+            if key not in ignore:
+                setattr(state, key, value)
+        storage.save()
+        return make_response(jsonify(state.to_dict()), 200)
