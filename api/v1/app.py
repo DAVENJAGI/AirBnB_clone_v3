8a94da9f52ec11c3204ee2407410f33377a1836b
DAVENJAGI <njagidave39@gmail.com>
2024-01-09 14:03:48 +0300

    Changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,21 +2,19 @@
 """creates a variable app, instance of flask, and tears down"""
 
 from flask import Flask
-# from api.v1.views import app_views, state_views, city_views
+from api.v1.views import app_views, state_views, city_views
 from api.v1.views import amenity_views, user_views
 from models import storage
 import os
 from flask import jsonify
 
 app = Flask(__name__)
-
-
 app.register_blueprint(app_views)
 app.register_blueprint(state_views)
 app.register_blueprint(city_views)
 app.register_blueprint(amenity_views)
 app.register_blueprint(user_views)
-
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exception=None):
