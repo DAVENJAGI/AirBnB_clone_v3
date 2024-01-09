de94eef5b2c16e5a601649966fe6bb22162a546f
DAVENJAGI <njagidave39@gmail.com>
2024-01-09 13:51:28 +0300

    changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """creates a variable app, instance of flask, and tears down"""
 
 from flask import Flask
-from views import app_views, state_views, city_views
+from api.v1.views import app_views, state_views, city_views
 from api.v1.views import amenity_views, user_views
 from models import storage
 import os
@@ -11,7 +11,6 @@ from flask import jsonify
 app = Flask(__name__)
 
 
-"""registration of bueprints"""
 app.register_blueprint(app_views)
 app.register_blueprint(state_views)
 app.register_blueprint(city_views)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,6 +9,7 @@ state_views = Blueprint("state_views", __name__, url_prefix="/api/v1")
 city_views = Blueprint("city_views", __name__, url_prefix="/api/v1")
 amenity_views = Blueprint("amenity_views", __name__, url_prefix="/api/v1")
 user_views = Blueprint("user_views", __name__, url_prefix="/api/v1")
+
 from views.index import *
 from views.states import *
 from views.cities import *
