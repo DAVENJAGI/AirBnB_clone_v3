965a9990b2ba9eabbad8342f38fec42225a02cf8
DAVENJAGI <njagidave39@gmail.com>
2024-01-09 03:33:54 +0300

    User_changes

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,67 +1,79 @@
 #!/usr/bin/python3
-"""
-script that uses the Flask framework to create an API for handling User objects
-"""
+"""import city_views and creates a route"""
 
-from flask import jsonify, abort, request
-from api.v1.views import app_views
+from flask import jsonify, Blueprint, abort, request
+from api.v1.views import user_views
 from models import storage
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
+import json
 
 
-# This route handles two types of HTTP requests: POST and GET
-@app_views.route('/users', methods=['GET', 'POST'],
-                 strict_slashes=False)
-def users():
-    """function handles two types of requests:
-        Retrieves all users
-    """
-    if request.method == 'GET':
-        user_list = []
-        for user in storage.all(User).values():
-            user_list.append(user.to_dict())
-        return jsonify(user_list)
-    if request.method == 'POST':
-        infos = request.get_json(silent=True)
-        if not infos:
-            abort(400, 'Not a JSON')
-        if 'email' not in infos:
-            abort(400, 'Missing email')
-        if 'password' not in infos:
-            abort(400, 'Missing password')
-        new_user = User(**infos)
+@user_views.route("/users", strict_slashes=False, methods=["GET"])
+def return_users():
+    """returns all state objects"""
+    all_users = storage.all(User).values()
+    users_list = []
+    for user in all_users:
+        users_list.append(user.to_dict())
+    return jsonify(users_list)
+
+
+@user_views.route("/users/<user_id>", strict_slashes=False,
+                     methods=["GET", "DELETE"])
+def return_user_by_id(user_id):
+    """Returns state based on city_id"""
+    if request.method == "GET":
+        all_users = storage.get(User, user_id)
+        if not all_users:
+            abort(404)
+        return jsonify(all_users.to_dict())
+
+    elif request.method == "DELETE":
+        user = storage.get(User, user_id)
+        if user is None:
+            abort(404)
+
+        storage.delete(user)
+        storage.save()
+        return jsonify({}), 200
+
+
+@user_views.route("/users", strict_slashes=False,
+                     methods=["POST"])
+def post_amenity():
+    """posts a new state"""
+    if request.method == "POST":
+        if not request.get_json():
+            abort(400, description="Not a JSON")
+        if 'email' not in request.get_json():
+            abort(400, description="Missing email")
+        if 'password' not in request.get_json():
+            abort(400, description="Missing password")
+
+        user_data = request.get_json()
+        new_user = User(**user_data)
         new_user.save()
         return jsonify(new_user.to_dict()), 201
 
 
-# This route handles three types of HTTP requests: DELETE, GET and POST
-@app_views.route('/users/<user_id>', methods=['DELETE', 'GET', 'PUT'],
-                 strict_slashes=False)
-def user_object(user_id):
-    """function handles three types of requests:
-    Handles a specified User
-    """
-    user = storage.get(User, user_id)
-    if user is None:
-        abort(404)
-    if request.method == 'DELETE':
-        for review in user.reviews:
-            storage.delete(review)
-        for place in user.places:
-            storage.delete(place)
-        storage.delete(user)
+@user_views.route("/users/<user_id>", methods=["PUT"])
+def update_city(user_id):
+    """updates data on a city"""
+    if request.method == "PUT":
+        all_users = storage.get(User, user_id)
+        if not all_users:
+            abort(404)
+        if not request.get_json():
+            abort(400, description="Not a JSON")
+        data = request.get_json()
+        for key, value in data.items():
+            if key not in ["id", "created_at", "updated_at"]:
+                setattr(all_users, key, value)
         storage.save()
-        return jsonify({}), 200
-    if request.method == 'GET':
-        return jsonify(user.to_dict())
-    if request.method == 'PUT':
-        infos = request.get_json(silent=True)
-        if not infos:
-            abort(400, 'Not a JSON')
-        for key, value in infos.items():
-            if key in ['id', 'created_at', 'updated_at', 'email']:
-                pass
-            else:
-                setattr(user, key, value)
-        user.save()
-        return jsonify(user.to_dict()), 200
+        return jsonify(all_users.to_dict()), 200
